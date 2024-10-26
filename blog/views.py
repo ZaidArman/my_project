d4b64388f4125b85e195b92c26391b593cfbbc81
ZaidArman <zahidu864@gmail.com>
2024-10-26 12:23:01 +0500

    kashaf chatbot

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-OPENAI_API_KEY = "sk-0iGd9K1i8Ct1LZiHZiziQO-LTuk9mZul5q6DUYo4obT3BlbkFJ7_oIC91mtgFq_JIIrhPCTkX-WuC2ciO1nYCr1bK3QA"
-
 def index(request):
     return render(request, 'blog/index.html')
 
